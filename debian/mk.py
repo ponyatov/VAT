02989ab328c74a0a9689a74da047a711da1c3792
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-08-29 18:19:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- debian/mk.py
+++ debian/mk.py
@@ -8,6 +8,22 @@
 
 import os,sys,time,re
 
+# current dir/project name
+CWD = re.findall(r'/([a-zA-Z_0-9]+)$',os.getcwd())[0]
+
+# target Makefile will be (re)created
+mk = open('Makefile','w')
+
+print >>mk,'CWD = $(notdir $(CURDIR))\n'
+
+# ffmpeg options
+print >>mk,'VIDEO = -vcodec libx264 -preset veryslow -qp 0 -crf 0 -bf 2 -flags +cgop -pix_fmt yuv420p\n'
+
+# default target
+print >>mk,'.PHONY: all\nall: $(CWD).mp4\n'
+
+MP4=[]
+
 # get all files with YYMMDD_HHMMSS pattern
 files = sorted(filter(lambda x:re.match(r'\d+_\d+\.',x),os.listdir('.')))
 #print files
@@ -19,5 +35,33 @@ for i in files:
 	ts = re.findall(r'\d+_\d+',i)[0]
 	try: F[ts] += [i]
 	except KeyError: F[ts] = [i]
-print F
+
+def dump():
+	for i in F:
+		print i
+		for j in F[i]:
+			print '\t',j
+
+#dump()
+
+for i in F:
+	if i+'.png'	in F[i]:
+		print >>mk,'\n%s.mp4: %s.png'%(i,i)
+		print >>mk,'\techo ffmpeg -i $< -r .2 $(VIDEO) -y $@'
+		MP4 += [i+'.mp4']
+	if i+'.en' in F[i] or i+'.ru' in F[i]:
+		print >>mk,'\n%s.mix.mp4: %s.mp4'%(i,i)
+		MP4 += [i+'.mix.mp4']
+
+print >>mk,'''
+FILE=180829_140652
+
+go:
+#	festival --tts $(FILE).en
+	festival --tts --language russian $(FILE).ru 
+'''
+
+print >>mk, '''
+$(CWD).mp4: %s
+'''% reduce(lambda a,b:a+' '+b,MP4)
 
